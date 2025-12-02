a63205df0eecb8fb08be2415c88ee0f949ff311a
Daniel Struthers <daniel.struthers@iqmetrix.com>
2025-12-02 13:11:00 -0800

    Add Puzzle2 implementation and tests
    Introduces Puzzle2 with solutions for PartOne and PartTwo, including logic for detecting repeating number patterns within ranges. Adds corresponding test cases and updates the project file to include Puzzle2 input files.

---
+++ AdventOfCode2025/Puzzle2/Puzzle.cs
@@ -0,0 +1,81 @@
+﻿namespace AdventOfCode2025.Puzzle2
+{
+    public static class Puzzle
+    {
+        private static string[][] GetColumns(string inputName) => File.ReadAllLines($"inputs/{inputName}")
+            .Select(x => x.Split(" ", StringSplitOptions.RemoveEmptyEntries)).ToArray();
+
+        public static long PartOne(string inputName)
+        {
+            long total = 0;
+            var input = GetRanges(inputName);
+
+            foreach (var range in input)
+            {
+                for (long i = range.Start; i <= range.End; i++)
+                {
+                    var numberStr = i.ToString();
+                    if(numberStr.Length %2 !=0) continue;
+                    var firstHalf= numberStr[..(numberStr.Length / 2)];
+                    var secondHalf = numberStr[(numberStr.Length / 2)..];
+                    if (firstHalf != secondHalf) continue;
+                    total += i;
+                }
+            }
+            return  total;
+        }
+
+        private static IEnumerable<(long Start, long End)> GetRanges(string inputName)
+        {
+            var columns = GetColumns(inputName);
+
+            var ranges = columns.SelectMany(x => x.SelectMany(y => y.Split(",", StringSplitOptions.RemoveEmptyEntries)));
+            var input = ranges.Select(x=>
+            {
+                var items = x.Split("-", StringSplitOptions.RemoveEmptyEntries);
+
+                return  (Start: long.Parse(items[0]), End: long.Parse(items[1]));
+            });
+            return input;
+        }
+
+
+        public static long PartTwo(string inputName)
+        {
+            long total = 0;
+            var input = GetRanges(inputName);
+
+            foreach (var range in input)
+            {
+                for (long i = range.Start; i <= range.End; i++)
+                {
+                    var numberStr = i.ToString();
+
+                    for (var j = 1; j <= numberStr.Length / 2; j++)
+                    {
+                        if(numberStr.Length % j != 0) continue;
+                        if (!IsRepeatingPattern(numberStr.AsSpan(), j)) continue;
+                        total += i;
+                        break;
+                    }
+
+                }
+            }
+            return  total;
+        }
+
+        private static bool IsRepeatingPattern(ReadOnlySpan<char> numberStr, int chunkSize)
+        {
+            var pattern = numberStr[..chunkSize];
+    
+            for (var i = chunkSize; i < numberStr.Length; i += chunkSize)
+            {
+                var chunk = numberStr.Slice(i, chunkSize);
+                if (!pattern.SequenceEqual(chunk))
+                    return false;
+            }
+    
+            return true;
+        }
+    }
+}
--- AdventOfCode2025/Puzzle2/Tests.cs
+++ AdventOfCode2025/Puzzle2/Tests.cs
@@ -1,22 +1,22 @@
-﻿namespace AdventOfCode2025.Puzzle1
+﻿namespace AdventOfCode2025.Puzzle2
 {
     [TestFixture]
     internal class Tests
     {
-        [TestCase("Puzzle1/sample.txt", 3)]
+        [TestCase("Puzzle2/sample.txt", 1227775554)]
 
-        [TestCase("Puzzle1/input.txt", 1023)]
-        public void PartA(string inputName, int answer)
+        [TestCase("Puzzle2/input.txt", 21898734247)]
+        public void PartA(string inputName, long answer)
         {
             var result = Puzzle.PartOne(inputName);
             Assert.That(result, Is.EqualTo(answer));
             Console.WriteLine(result);
         }
 
-        [TestCase("Puzzle1/sample.txt", 6)]
+        [TestCase("Puzzle2/sample.txt",  4174379265)]
 
-        [TestCase("Puzzle1/input.txt", 5899)]
-        public void PartB(string inputName, int answer)
+        [TestCase("Puzzle2/input.txt", 28915664389)]
+        public void PartB(string inputName, long answer)
         {
             var result = Puzzle.PartTwo(inputName);
             Assert.That(result, Is.EqualTo(answer));
