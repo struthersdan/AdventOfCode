74993fad4758a1b5344d288951e825f23c8ca4fb
Daniel Struthers <daniel.struthers@iqmetrix.com>
2025-12-03 09:22:20 -0800

    Add Puzzle 3 solution and tests
    Implemented the solution for Puzzle 3 with methods for PartOne and PartTwo in Puzzle3/Puzzle.cs. Added corresponding unit tests in Puzzle3/Tests.cs and updated the project file to include input files for Puzzle 3.

---
+++ AdventOfCode2025/Puzzle3/Puzzle.cs
@@ -0,0 +1,68 @@
+﻿namespace AdventOfCode2025.Puzzle3
+{
+    public static class Puzzle
+    {
+        private static string[][] GetColumns(string inputName) => File.ReadAllLines($"inputs/{inputName}")
+            .Select(x => x.Split(" ", StringSplitOptions.RemoveEmptyEntries)).ToArray();
+
+        public static long PartOne(string inputName)
+        {
+            return Solve(inputName, 2);
+        }
+
+        public static long PartTwo(string inputName)
+        {
+            return  Solve(inputName, 12);
+        }
+
+        private static long Solve(string inputName, int length)
+        {
+            var columns = GetColumns(inputName);
+            long total = 0;
+            foreach (var column in columns)
+            {
+                var numbers = column[0].Select(x => int.Parse(x.ToString())).ToArray();
+                total += long.Parse(GetJoltage(numbers, length));
+            }
+
+            return total;
+        }
+
+        private static string GetJoltage(int[] numbers, int length)
+        {
+            var joltage = "";
+            var start = 0;
+
+            for (var i = 0; i < length; i++)
+            {
+                var (largest, nextStart) = GetNextLargestNumber(numbers, start, length - 1 - i);
+                joltage += largest.ToString();
+                start = nextStart;
+            }
+
+            return joltage;
+        }
+
+        private static (int largest, int nextStart) GetNextLargestNumber(int[] numbers, int start, int endBuffer)
+        {
+            var largest = 0;
+            var nextStart = 0;
+            for (var i = start; i < numbers.Length - endBuffer; i++)
+            {
+                var next = numbers[i];
+                if (next > largest)
+                {
+                    largest = next;
+                    nextStart = i + 1;
+                }
+
+                if (next == 9)  break;
+            }
+
+            return (largest, nextStart);
+        }
+
+
+   
+    }
+}
---
+++ AdventOfCode2025/Puzzle3/Tests.cs
@@ -0,0 +1,25 @@
+﻿namespace AdventOfCode2025.Puzzle3
+{
+    [TestFixture]
+    internal class Tests
+    {
+        const string PuzzleName = "Puzzle3";
+        [TestCase($"{PuzzleName}/sample.txt", 357)]
+        [TestCase($"{PuzzleName}/input.txt", 17376)]
+        public void PartA(string inputName, long answer)
+        {
+            var result = Puzzle.PartOne(inputName);
+            Assert.That(result, Is.EqualTo(answer));
+            Console.WriteLine(result);
+        }
+
+        [TestCase($"{PuzzleName}/sample.txt", 3121910778619)]
+        [TestCase($"{PuzzleName}/input.txt", 172119830406258)]
+        public void PartB(string inputName, long answer)
+        {
+            var result = Puzzle.PartTwo(inputName);
+            Assert.That(result, Is.EqualTo(answer));
+            Console.WriteLine(result);
+        }
+    }
+}
