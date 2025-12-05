cb1bd6bdc46efe0bf79dc279ac205106cf5801a1
Daniel Struthers <daniel.struthers@iqmetrix.com>
2025-12-05 14:15:25 -0800

    Add Puzzle 5 solution and tests
    Implements the solution for Puzzle 5 with methods for both parts, including range merging logic. Adds corresponding unit tests and updates the project file to include input files for Puzzle 5.

---
+++ AdventOfCode2025/Puzzle5/Puzzle.cs
@@ -0,0 +1,73 @@
+﻿namespace AdventOfCode2025.Puzzle5;
+
+public static class Puzzle
+{
+    private static string[][] GetColumns(string inputName)
+    {
+        return File.ReadAllLines($"inputs/{inputName}")
+            .Select(x => x.Split(" ", StringSplitOptions.RemoveEmptyEntries)).ToArray();
+    }
+
+    public static long PartOne(string inputName)
+    {
+        var columns = GetColumns(inputName);
+        var freshMap = BuildFreshRanges(columns);
+        return CheckIngredients(columns.SkipWhile(t => t.Length != 0).Skip(1).Select(x => long.Parse(x[0])), freshMap);
+    }
+
+    public static long PartTwo(string inputName)
+    {
+        var columns = GetColumns(inputName);
+        var freshMap = BuildFreshRanges(columns);
+        var distinctRanges = MergeRanges(freshMap);
+
+
+        return distinctRanges.Sum(valueTuple => valueTuple.max - valueTuple.min + 1);
+    }
+
+    private static int CheckIngredients(IEnumerable<long> ingredients, List<(long Min, long Max)> freshMap)
+    {
+        return ingredients.Count(ingredient =>
+            freshMap.Any(range => ingredient >= range.Min && ingredient <= range.Max));
+    }
+
+    private static List<(long Min, long Max)> BuildFreshRanges(string[][] columns)
+    {
+        return (from t in columns.TakeWhile(t => t.Length != 0)
+            select t[0].Split('-')
+            into range
+            let min = long.Parse(range[0])
+            let max = long.Parse(range[1])
+            select (min, max)).ToList();
+    }
+
+    private static IEnumerable<(long min, long max)> MergeRanges(List<(long min, long max)> freshMap)
+    {
+        for (var index = 0; index < freshMap.Count; index++)
+        {
+            var (min, max) = freshMap[index];
+
+            for (var i = 0; i < freshMap.Count; i++)
+            {
+                if (index == i) continue;
+                var (otherMin, otherMax) = freshMap[i];
+                otherMin = SetOtherMin(otherMin, min, max);
+                otherMax = SetOtherMax(otherMax, max, min);
+
+                freshMap[i] = (otherMin, otherMax);
+            }
+        }
+
+        return freshMap.Distinct();
+    }
+
+    private static long SetOtherMax(long otherMax, long max, long min)
+    {
+        return  (otherMax < max && otherMax >= min) ? max : otherMax;       
+    }
+
+    private static long SetOtherMin(long otherMin, long min, long max)
+    {
+        return (otherMin > min && otherMin <= max) ? min : otherMin;
+    }
+}
---
+++ AdventOfCode2025/Puzzle5/Tests.cs
@@ -0,0 +1,25 @@
+﻿namespace AdventOfCode2025.Puzzle5
+{
+    [TestFixture]
+    internal class Tests
+    {
+        const string PuzzleName = "Puzzle5";
+        [TestCase($"{PuzzleName}/sample.txt", 3)]
+        [TestCase($"{PuzzleName}/input.txt", 840)]
+        public void PartA(string inputName, long answer)
+        {
+            var result = Puzzle.PartOne(inputName);
+            Assert.That(result, Is.EqualTo(answer));
+            Console.WriteLine(result);
+        }
+
+        [TestCase($"{PuzzleName}/sample.txt", 14)]
+        [TestCase($"{PuzzleName}/input.txt", 359913027576322)]
+        public void PartB(string inputName, long answer)
+        {
+            var result = Puzzle.PartTwo(inputName);
+            Assert.That(result, Is.EqualTo(answer));
+            Console.WriteLine(result);
+        }
+    }
+}
